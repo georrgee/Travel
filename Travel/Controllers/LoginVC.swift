bfa74a612ade9f7d9090517ac499640a0fb849f8
georrgee <garcia.george.b@gmail.com>
2020-03-18 15:38:53 -0700

    Setting up LoginVC UI

---
+++ Travel/Controllers/LoginVC.swift
@@ -0,0 +1,16 @@
+//  LoginVC.swift
+//  Travel
+//  Created by George Garcia on 3/18/20.
+//  Copyright © 2020 GeeTeam. All rights reserved.
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .purple
+    }
+    
+}
